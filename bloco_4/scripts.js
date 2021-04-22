f9b56d85c0953c09d81bf272a2af255418687bc5
Lucas A. Santos <lucasquearis@hotmail.com>
2021-04-22 18:42:30 -0300

    Fim da parte V exercicios

--- bloco_4/scripts.js
+++ bloco_4/scripts.js
@@ -32,3 +32,25 @@ if (nota >= 80 ){
 } else {
     return;
 }
+
+// Parte V
+
+let estadoAprovacao = 'oi';
+
+switch (estadoAprovacao) {
+    case 'aprovada':
+        console.log('Parabéns, você foi aprovada(o)');
+        break;
+    
+    case 'lista':
+        console.log('Você está na nossa lista de espera');
+        break;
+
+    case 'reprovada':
+        console.log('Você foi reprovada(o)');
+        break;
+
+    default:
+        console.log('Não se aplica');
+        break;
+}
