f1c616416828e1ee5535a3cb5e3d1dd80f70fbce
Lucas A. Santos <lucasquearis@hotmail.com>
2021-04-23 10:39:50 -0300

    exercicio de fixacao parte IV feito

--- Bloco 4: Introdução - JavaScript/dia_1/script.js
+++ Bloco 4: Introdução - JavaScript/dia_1/script.js
@@ -34,3 +34,15 @@ console.log(area);
 let perimetro = ((2*altura)+(2*base))
 
 console.log(perimetro)
+
+let nota = 60;
+
+if (nota>=80){
+    console.log('Parabéns, você foi aprovada(o)!');
+} else if (nota<80&&nota>=60){
+    console.log('Você está na nossa lista de espera')
+} else if (nota <60){
+    console.log('Você foi reprovada(o)')
+} else {
+    return;
+}
