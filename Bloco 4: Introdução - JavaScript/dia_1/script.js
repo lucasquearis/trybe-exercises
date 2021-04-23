28a2f79670c89b5427354ba5925e5c6f8a764b7d
Lucas A. Santos <lucasquearis@hotmail.com>
2021-04-23 10:53:33 -0300

    exercicio de fixacao parte V feito

--- Bloco 4: Introdução - JavaScript/dia_1/script.js
+++ Bloco 4: Introdução - JavaScript/dia_1/script.js
@@ -46,3 +46,23 @@ if (nota>=80){
 } else {
     return;
 }
+
+let estado = "batata";
+
+switch (estado){
+    case"aprovada":
+        console.log('Parabéns, você foi aprovada(o)!');
+        break;
+
+    case"lista":
+        console.log('Você está na nossa lista de espera')
+        break;
+
+    case"reprovada":
+        console.log('Você foi reprovada(o)')
+        break;
+
+    default:
+        console.log('Não se aplica')
+        break;
+}
