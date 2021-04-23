5b5fcc8a2644e533c7b048980476d3df897589fe
Lucas A. Santos <lucasquearis@hotmail.com>
2021-04-23 10:32:11 -0300

    exercicio de fixacao dia 1 feito

--- Bloco 4: Introdução - JavaScript/dia_1/script.js
+++ Bloco 4: Introdução - JavaScript/dia_1/script.js
@@ -13,3 +13,24 @@ console.log(name);
 console.log(birthCity);
 
 console.log(birthYear);
+
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+let base = 5;
+
+let altura = 8;
+
+let area = (base*altura);
+
+console.log(area);
+
+let perimetro = ((2*altura)+(2*base))
+
+console.log(perimetro)
