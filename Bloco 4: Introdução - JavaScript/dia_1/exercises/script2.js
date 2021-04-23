5cc66c3cd3d4f47b279284eadeabdf053b616759
Lucas A. Santos <lucasquearis@hotmail.com>
2021-04-23 11:17:13 -0300

    Script 2 feito

---
+++ Bloco 4: Introdução - JavaScript/dia_1/exercises/script2.js
@@ -0,0 +1,15 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let numeroA = 289;
+let numeroB = 261;
+
+let maiorNumero;
+
+if (numeroA>numeroB) {
+    maiorNumero = numeroA
+} else {
+    maiorNumero = numeroB
+}
+console.log('O maior número entre numeroA e numeroB é:')
+console.log(maiorNumero);
+
