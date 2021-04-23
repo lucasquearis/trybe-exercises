05d36150ad1015a5ec05cd32c1b3caca21d71661
Lucas A. Santos <lucasquearis@hotmail.com>
2021-04-23 10:25:56 -0300

    Exercicio fixacao parte 2 feito

---
+++ Bloco 4: Introdução - JavaScript/dia_1/script.js
@@ -0,0 +1,15 @@
+let name ='Lucas';
+
+const birthCity ='Curitiba';
+
+let birthYear =1996;
+
+birthYear = 2030
+
+birthCity = 'São Paulo'
+
+console.log(name);
+
+console.log(birthCity);
+
+console.log(birthYear);
