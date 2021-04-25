19e5638a3ba23bbba64c1e92fc8126a9e5fadce4
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-04-25 17:41:45 -0300

    Exercicios adicionado comentarios e 8 9 finalizados

--- Bloco 4: Introdução - JavaScript/dia_2/exercises/script5.js
+++ Bloco 4: Introdução - JavaScript/dia_2/exercises/script5.js
@@ -1,3 +1,6 @@
+//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let maiorNumero = numbers[0];
--- Bloco 4: Introdução - JavaScript/dia_2/exercises/script7.js
+++ Bloco 4: Introdução - JavaScript/dia_2/exercises/script7.js
@@ -1,3 +1,5 @@
+//Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let menorNumero = numbers[0];
