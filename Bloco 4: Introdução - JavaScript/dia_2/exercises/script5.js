09ee7aa3e432b317431712b9fa895ed066fba6f8
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-04-25 17:10:40 -0300

    TERMINEI O EXERCICIO 5 ALELUIAA

---
+++ Bloco 4: Introdução - JavaScript/dia_2/exercises/script5.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let maiorNumero = numbers[0];
+
+for(let index = 0; index < numbers.length; index+=1) {
+  if(numbers[index]>maiorNumero){
+    maiorNumero=numbers[index]
+  }
+}
+console.log(maiorNumero);
