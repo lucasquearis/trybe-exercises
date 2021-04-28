0a081527008aacf355d642a0c7596ea0d0d0a949
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-04-28 18:00:51 -0300

    Exercicio de refatoracao 1 exercicio feito

---
+++ Bloco 4: Introdução - JavaScript/dia_4/refatorar.js
@@ -0,0 +1,10 @@
+let valorA = 5;
+let valorB = 5;
+
+//Script de soma//
+function operacaoSoma(valorA, valorB){
+  let resultado = valorA + valorB;
+  console.log(resultado);
+}
+
+operacaoSoma(valorA, valorB);
