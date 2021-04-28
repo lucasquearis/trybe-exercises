b69e8eb2b0663806bfc14f41ef3e95f17aaca26e
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-04-28 18:12:28 -0300

    Exercicio de refatoracao exercicios feitos

--- Bloco 4: Introdução - JavaScript/dia_4/refatorar.js
+++ Bloco 4: Introdução - JavaScript/dia_4/refatorar.js
@@ -1,10 +1,45 @@
 let valorA = 5;
 let valorB = 5;
 
+console.log('valor A é = ' + valorA + ' valor B é = ' + valorB)
+
 //Script de soma//
-function operacaoSoma(valorA, valorB){
+function operacaoSoma(){
   let resultado = valorA + valorB;
-  console.log(resultado);
+  console.log('Resultado soma ' + resultado);
 }
 
 operacaoSoma(valorA, valorB);
+
+//Script de Subtração
+
+function operacaoSubtracao(){
+  let resultado= valorA - valorB;
+  console.log('Resultado Subtração ' + resultado);
+}
+
+operacaoSubtracao(valorA, valorB)
+
+//Script de Multiplicação
+
+function operacaoMultiplicacao(){
+  let resultado = valorA * valorB;
+  console.log('Resultado Multiplicação ' + resultado);
+}
+operacaoMultiplicacao(valorA, valorB)
+
+//Script de Divisão
+
+function operacaoDivisao(){
+  let resultado = valorA / valorB;
+  console.log('Resultado Divisão ' + resultado);
+}
+operacaoDivisao(valorA, valorB);
+
+//Script de Módulo
+
+function operacaoModulo(){
+  let resultado = valorA % valorB;
+  console.log('Resultado Módulo ' + resultado);
+}
+operacaoModulo(valorA, valorB);
