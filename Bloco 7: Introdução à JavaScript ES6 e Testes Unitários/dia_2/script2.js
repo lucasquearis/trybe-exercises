b9d29c1101b4ae959d0a0f059f1d9049739d50f1
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-20 18:18:50 -0300

    exercicios de fixacao feitos

---
+++ Bloco 7: Introdução à JavaScript ES6 e Testes Unitários/dia_2/script2.js
@@ -0,0 +1,14 @@
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
