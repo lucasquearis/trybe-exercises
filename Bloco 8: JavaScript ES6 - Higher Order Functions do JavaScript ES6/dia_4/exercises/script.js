e171402c718ab8dd13e79b217d997728f0f5e9c6
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-31 18:45:40 -0300

    dia 4 feito

---
+++ Bloco 8: JavaScript ES6 - Higher Order Functions do JavaScript ES6/dia_4/exercises/script.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  // escreva seu código aqui
+  arrays.reduce()
+flatten();
+
+// assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
