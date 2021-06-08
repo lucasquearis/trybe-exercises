10c8ee5cb6da413c006099f3516c19342fa905ac
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-08 06:22:19 -0300

    exercicio 6 feito

---
+++ Bloco 8: JavaScript ES6 - Higher Order Functions do JavaScript ES6/dia_4/exercises/script6.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  // escreva seu código aqui
+  return students.map((student, index) => ({
+    name: student,
+    average: grades[index].reduce((acc, current) =>  acc + current,0) / grades[index].length,
+  }));
+}
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
