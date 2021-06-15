61e070365ae7da34110c3e24545f18bbb8dff978
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-06-15 20:28:00 -0300

    exercicios 2 e 3 finalizados

--- Bloco 10 - Testes automatizados com Jest/dia_2/exercises/exercicio2e3.test.js
+++ Bloco 10 - Testes automatizados com Jest/dia_2/exercises/exercicio2e3.test.js
@@ -1,1 +1,28 @@
-const { users, findUserById, getUserName } = require('./exercicio2e3')
+const { users, findUserById, getUserName } = require('./exercicio2e3');
+
+describe('Testa funcao getUserName', () => {
+  test('Verifica resultado da funcao' , () => {
+    expect.assertions(1);
+    return getUserName(4).then((data) => expect(data).toEqual('Mark'))
+  })
+
+  test('Verifica erro na funcao', () => {
+    expect.assertions(1);
+    return getUserName(1).catch((error) => expect(error).toEqual({ error: 'User with 1 not found.' }))
+  })
+  
+  test('Verifica resultado da funcao' , async () => {
+    expect.assertions(1);
+    const data = await getUserName(4);
+    expect(data).toEqual('Mark')
+  })
+
+  test('Verifica erro na funcao Async/Await', async () => {
+    expect.assertions(1);
+    try {
+      await getUserName(2)
+    } catch(error) {
+      expect(error).toEqual({ error: 'User with 2 not found.' });
+    }
+  })
+});
