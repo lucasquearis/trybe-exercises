4d68bdc04739f0a5fe90a0459521f41071418943
lucasquearis <lucasquearis@hotmail.com>
2021-11-03 18:59:30 -0300

    feito menu de script com npm start

--- Módulo_3-Desenvolvimento_Back-end/Introducao_ao_desenvolvimento_Web_com_NodeJS/dia_1/exercicios/my-scripts/index.js
+++ Módulo_3-Desenvolvimento_Back-end/Introducao_ao_desenvolvimento_Web_com_NodeJS/dia_1/exercicios/my-scripts/index.js
@@ -1,1 +1,26 @@
-console.log('hello')
+const { questionInt } = require('readline-sync');
+
+console.log(
+  `
+    Olá bem vindos ao meu script, escolha o que quer ver:
+    1- Calcule seu IMC.
+    2- Calculadora Velocidade Média.
+    3- Sorteio.
+
+  `
+)
+const questao = questionInt('Qual a sua opção?  ');
+
+switch (questao) {
+  case 1:
+    require('./imc.js')
+    break;
+  case 2:
+    require('./velocidade.js')
+    break;
+  case 3:
+    require('./sorteio')
+    break;
+  default:
+    console.log('escolha uma opção valida!')
+}
