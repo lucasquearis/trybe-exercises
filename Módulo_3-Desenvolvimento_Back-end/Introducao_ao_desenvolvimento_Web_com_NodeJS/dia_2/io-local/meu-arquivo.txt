858b75de8bb5514281ebe095e82529bec038d56b
lucasquearis <lucasquearis@hotmail.com>
2021-11-04 14:09:41 -0300

    exercicios fixaçao feitos bloco 25 dia 2

---
+++ Módulo_3-Desenvolvimento_Back-end/Introducao_ao_desenvolvimento_Web_com_NodeJS/dia_2/io-local/meu-arquivo.txt
@@ -0,0 +1,1 @@
+Meu textão
---
+++ Módulo_3-Desenvolvimento_Back-end/Introducao_ao_desenvolvimento_Web_com_NodeJS/dia_2/io-local/writeFile.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+
+fs.writeFile('./meu-arquivo.txt', 'Meu textão')
+  .then(() => {
+    console.log('Arquivo escrito com sucesso!');
+  })
+  .catch((err) => {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  });
