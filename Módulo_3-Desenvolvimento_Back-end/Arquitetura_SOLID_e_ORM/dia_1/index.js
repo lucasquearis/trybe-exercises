cd6c85705783ee18728563f22e0a7875317ae4d5
lucasquearis <lucasquearis@hotmail.com>
2022-01-04 11:09:12 -0300

    exercicios fixacao dia 29.1

---
+++ Módulo_3-Desenvolvimento_Back-end/Arquitetura_SOLID_e_ORM/dia_1/index.js
@@ -0,0 +1,83 @@
+// ./index.js
+const GRADE_DICT = {
+  0.9: 'A',
+  0.8: 'B',
+  0.7: 'C',
+  0.6: 'D',
+  0.1: 'E',
+};
+
+const gradeKeys = Object.keys(GRADE_DICT);
+
+/* Função menor para remover o uso excessivo de `if{}else`s */
+const getGradeLetter = (gradeNumber) => {
+  let letterGrade = 'F';
+
+  for (let i = 0; i < gradeKeys.length; i += 1) {
+    if (gradeNumber >= gradeKeys[i]) {
+      letterGrade = GRADE_DICT[gradeKeys[i]];
+      break;
+    }
+  }
+
+  return letterGrade;
+};
+
+/* Coletar notas */
+const getLetterGrades = ({ name, grade }) => ({
+  name,
+  grade,
+  letterGrade: getGradeLetter(grade) });
+
+/* "Converter" */
+const percentageGradesIntoLetters = ({ name, disciplines, school }) => ({
+  name,
+  school,
+  disciplines: disciplines.map(getLetterGrades) });
+
+/* "Determinar" */
+const approvedStudents = ({ disciplines }) =>
+  disciplines.every(({ grade }) => grade > 0.7);
+
+/* "Atualizar" */
+const updateApprovalData = ({ name: studentName, disciplines }) => {
+  console.log(`A pessoa com nome ${studentName} foi aprovada!`);
+
+  disciplines.map(({ name, letterGrade }) =>
+    console.log(`${name}: ${letterGrade}`));
+};
+
+function setApproved(students) {
+  students
+    .map(percentageGradesIntoLetters)
+    .filter(approvedStudents)
+    .map(updateApprovalData);
+}
+
+/* Abaixo temos um exemplo de execução */
+const students = [
+  {
+    name: 'Lee',
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.6 },
+    ],
+  },
+  {
+    name: 'Clementine',
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.9 },
+    ],
+  },
+];
+
+setApproved(students);
+
+module.exports = {
+  percentageGradesIntoLetters,
+  approvedStudents,
+  updateApprovalData,
+  setApproved,
+  getLetterGrades,
+};
