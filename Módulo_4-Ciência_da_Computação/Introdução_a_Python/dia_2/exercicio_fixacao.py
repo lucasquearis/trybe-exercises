1350c04c5edaf44a6a9f4330896e7def8885e371
lucasquearis <lucasquearis@hotmail.com>
2022-02-09 17:56:39 -0300

    feito exercicios fixacao parte 1 dia 32.2

---
+++ Módulo_4-Ciência_da_Computação/Introdução_a_Python/dia_2/exercicio_fixacao.py
@@ -0,0 +1,9 @@
+import random
+
+random_number = random.randint(1, 10)  # escolhe um número aleatório entre 1 e 10
+guess = ""
+
+while guess != random_number:  # enquanto não adivinhar o número
+    guess = int(input("Qual o seu palpite? "))  # pergunte a pessoa usuária um número
+
+print("O número sorteado era: ", guess)
