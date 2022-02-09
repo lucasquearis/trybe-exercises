a34be544eae85c327fb01b8e9973f734ed21336b
lucasquearis <lucasquearis@hotmail.com>
2022-02-09 15:13:00 -0300

    feito exercicios fixacao parte 2 dia 32.1

---
+++ Módulo_4-Ciência_da_Computação/Introdução_a_Python/dia_1/exercicios_ficaxao_two.py
@@ -0,0 +1,22 @@
+a = 5
+print(type(a))
+
+b = 5.0
+print(type(b))
+
+c = 5j
+print(type(c))
+
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+
+trybe_course.append("Ciência da Computação")
+
+trybe_course[0] = "Fundamentos"
+
+print(trybe_course)
+
+var = set()
+
+var.add("Lucas")
+
+print(var)
