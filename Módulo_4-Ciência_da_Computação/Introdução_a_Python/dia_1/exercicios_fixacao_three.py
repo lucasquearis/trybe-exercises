f272a3f9f2f6210839305f0bde8a5074922377dc
lucasquearis <lucasquearis@hotmail.com>
2022-02-09 21:06:31 -0300

    feito exercicios fixacao parte 3 dia 32.1

---
+++ Módulo_4-Ciência_da_Computação/Introdução_a_Python/dia_1/exercicios_fixacao_three.py
@@ -0,0 +1,14 @@
+info = {
+  "personagem": "Margarida",
+  "origem": "Pato Donald",
+  "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
+}
+
+info["recorrente"] = "Sim"
+
+print(info)
+
+del info["origem"]
+
+print(info)
+
