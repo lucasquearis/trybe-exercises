fc9c7e54c7945eb991afdbc8239e0c5fe3ff223e
lucasquearis <lucasquearis@hotmail.com>
2022-02-13 15:52:24 -0300

    termina exercicios fixacao dia 1

---
+++ Módulo_4-Ciência_da_Computação/Introdução_a_Python/dia_1/people.py
@@ -0,0 +1,10 @@
+import area
+
+
+PEOPLE_AT_CONCERT_PER_SQUARE_METER = 2
+FIELD_LENGTH = 240
+FIELD_WIDTH = 45
+PEOPLE_AT_CONCERT = area.rectangle(FIELD_LENGTH, FIELD_WIDTH) // PEOPLE_AT_CONCERT_PER_SQUARE_METER
+
+
+print("Estão presentes no show aproximadamente", PEOPLE_AT_CONCERT, "pessoas")
